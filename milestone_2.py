3871c5dd367781ec2c7fb7f180a68ceae51b57d2
Ali <alistair.p.watson@gmail.com>
2023-11-28 16:17:45 +0000

    Updated code in milestone_2.py, adds milestone_3.py, which includes functions for asking user for input and checking validity of guess

--- milestone_2.py
+++ milestone_2.py
@@ -4,11 +4,13 @@ word_list = ["strawberry", "raspberry", "orange", "kiwi", "cherry"]
 
 word = random.choice(word_list)
 
-guess = (input("Please enter a single letter:"))
+if __name__ == '__main__':
+
+    guess = (input("Please enter a single letter:"))
 
 
-if len(guess.lower()) == 1 and guess.lower() in "abcdefghijklmnopqrstuvwxyz":
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input")
+    if len(guess.lower()) == 1 and guess.lower().isalpha():
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input")
 
