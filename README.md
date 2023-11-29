3fdd09cfa1212d532d563d773545c6c120949997
Ali <alistair.p.watson@gmail.com>
2023-11-29 16:59:06 +0000

    Adds milestone_5.py, which includes the previous code and adds the play_game function. Amended README.md to include instructions on how to run the code

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Hangman
 Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
 
-This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
+This is an implementation of the Hangman game, where the computer chooses a random word from a list and the user tries to guess it. 
+
+The code is run using the play_game function in milestone_5.py, using the desired list of words as the argument. The player has 5 lives, so if 5 guesses are incorrect, the game is over.
---
+++ milestone_5.py
@@ -0,0 +1,52 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for character in self.word]
+        self.num_letters = len(set(self.word))
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        # Checks if guessed letter is in the secret word
+        guess_lower = guess.lower()
+        self.word = self.word.lower()
+        if guess_lower in self.word:
+            print(f"Good guess! '{guess}' is in the word!")
+            for i, character in enumerate(self.word):
+                if guess_lower == character:
+                    self.word_guessed[i] = character
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, '{guess}' is not in the word. Try again.")
+            print(f"You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+         # Asks for a letter and checks if it is a valid input, and adds the guess to the list of guesses
+            guess = input("Choose a letter:")
+            if not (len(guess) == 1 and guess.isalpha()):
+                print("Invalid choice. Please enter a single alphabetic character.")
+            elif guess in self.list_of_guesses:
+                print(f"You already tried '{guess}'.")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
+
+
+def play_game(word_list):
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+    while True:
+        if game.num_lives == 0:
+            print(f"Unlucky, the word was '{game.word}'. You lose, good day sir!")
+            break
+        elif game.num_letters > 0:
+            game.ask_for_input()
+        else:
+            print(f"Congratulations. The word was '{game.word}'. You won the game!")
+            break
