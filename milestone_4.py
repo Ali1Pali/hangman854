937c7fff4bedd16eee0669a615ac437c8cc0c0d1
Ali <alistair.p.watson@gmail.com>
2023-11-29 15:11:28 +0000

    Minor code clean up in milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -16,22 +16,28 @@ class Hangman:
         guess = guess.lower()
         self.word = self.word.lower()
         if guess in self.word:
-            print(f"Good guess! '{guess}' is in the word")
+            print(f"Good guess! '{guess}' is in the word!")
+            for i, character in enumerate(self.word):
+                if guess == character:
+                    self.word_guessed[i] = character
+            self.num_letters -= 1
         else:
-            print(f"Sorry, '{guess}' is not in the word. Try again")
+            self.num_lives -= 1
+            print(f"Sorry, '{guess}' is not in the word. Try again.")
+            print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
-         # Asks for a letter and checks if it is a valid input
+         # Asks for a letter and checks if it is a valid input, and adds the guess to the list of attempts
         while True:
             guess = input("Choose a letter:")
             if not (len(guess) == 1 and guess.isalpha()):
-                print("Invalid letter. Please enter a single alphabetic character")
+                print("Invalid choice. Please enter a single alphabetic character.")
             elif guess in self.list_of_guesses:
-                print(f"You already tried '{guess}'")
+                print(f"You already tried '{guess}'.")
             else:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
 
-hangman_attempt = Hangman(["strawberry", "raspberry", "orange", "kiwi", "cherry"])
+# hangman_attempt = Hangman(["strawberry", "raspberry", "orange", "kiwi", "cherry"])
 
-hangman_attempt.ask_for_input()
+# hangman_attempt.ask_for_input()
